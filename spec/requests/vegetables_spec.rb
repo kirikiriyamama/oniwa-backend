8e9aa248dad6a0005c2e95b7f562b15eb0af3318
lime1024 <lime1024@gmail.com>
2020-05-24 14:25:19 +0900

    RSpec のリファクタリング

--- spec/requests/vegetables_spec.rb
+++ spec/requests/vegetables_spec.rb
@@ -5,8 +5,14 @@ RSpec.describe 'Vegetable' do
         post vegetables_path, params: { name: 'ブロッコリー' }
       }.to change(Vegetable, :count).by(1)
       expect(response).to have_http_status(200)
-      expect(JSON.parse(response.body)['vegetable']['id']).to eq Vegetable.last.id
-      expect(JSON.parse(response.body)['vegetable']['name']).to eq 'ブロッコリー'
+      expect(JSON.parse(response.body).deep_symbolize_keys).to eq(
+        {
+          vegetable: {
+            id: Vegetable.last.id,
+            name: 'ブロッコリー'
+          }
+        }
+      )
     end
 
     context '野菜の名前が nil の場合' do
@@ -15,7 +21,11 @@ RSpec.describe 'Vegetable' do
           post vegetables_path, params: { name: nil }
         }.not_to change(Vegetable, :count)
         expect(response).to have_http_status(400)
-        expect(JSON.parse(response.body)['errors'][0]['message']).to eq '野菜の名前を入力してください'
+        expect(JSON.parse(response.body).deep_symbolize_keys).to eq(
+          errors: [
+            { message: '野菜の名前を入力してください' }
+          ]
+        )
       end
     end
   end
